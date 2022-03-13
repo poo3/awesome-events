e8a947fcccff4a6523eb797b63f20ccfd5818b0f
バントウフウマ <coolishpoo3@gmail.com>
2022-03-13 18:43:32 +0900

    userがgithub認証でログイン・ログアウトできるようにしました！！

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  root 'welcome#index'
+  delete '/logout' => 'sessions#destroy'
+  get '/auth/:provider/callback' => 'sessions#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
